b272136952e32d5620269bc9f887242792554dbe
Maui <mauiazryl0825@gmail.com>
2024-06-01 13:50:51 +0800

    Update mainLayout.tsx

--- src/layout/mainLayout.tsx
+++ src/layout/mainLayout.tsx
@@ -1,22 +1,19 @@
-import React from 'react'
-import Header from '@/components/navbar'
-import { ReactNode} from 'react';
+import React from 'react';
+import Header from '@/components/navbar';
+import { ReactNode } from 'react';
 import '../app/globals.css';
 
 interface MainProps {
   children: ReactNode;
 }
 
+const Main = ({ children }: MainProps) => {
+  return (
+    <div className="flex flex-col items-center min-h-screen">
+      <Header />
+      <main className="w-1/2 flex items-center">{children}</main>
+    </div>
+  );
+};
 
-const Main = ({children}: MainProps) => {
-    return (
-      <main className="flex min-h-screen flex-col items-center">
-        <Header></Header>
-        <div className="w-1/2 flex min-h-96 items-center">
-          {children}
-        </div>
-      </main>
-    );
-  };
-  
-export default Main
+export default Main;
